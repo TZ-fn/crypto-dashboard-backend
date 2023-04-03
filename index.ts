202464798f63a4b39b16e9b4ebf014c582efeef0
Tomek <tomekz@vp.pl>
2023-04-03 21:25:22 +0200

    fix: change the app's port

--- index.ts
+++ index.ts
@@ -6,9 +6,10 @@ dotenv.config();
 const app = express();
 
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.get("/", async (_req, res) => {
+  console.log("get");
   const data = await getData(COINMARKETCAP_API_KEY);
   if (data) {
     res.send(await data.json());
@@ -18,5 +19,3 @@ app.get("/", async (_req, res) => {
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
-
-//
