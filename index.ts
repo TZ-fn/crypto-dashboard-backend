55b557df0860ee613ecc0488467ec72985212393
Tomek <tomekz@vp.pl>
2023-04-04 17:44:13 +0200

    feat: create a file for routes

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import * as dotenv from "dotenv";
 import express from "express";
 import getData from "./getData.ts";
+import getRoute from "./getRoute.js";
+import routes from "./routes.ts";
 
 dotenv.config();
 const app = express();
@@ -8,12 +10,11 @@ const app = express();
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 const PORT = process.env.PORT || 3001;
 
-app.get("/", async (_req, res) => {
+app.get(getRoute(routes.latest), async (_req, res) => {
   const data = await getData(COINMARKETCAP_API_KEY);
   if (data) {
     res.type("json").send(await data.json());
   }
-  console.log(data);
 });
 
 app.listen(PORT, () => {
---
+++ routes.ts
@@ -0,0 +1,5 @@
+const routes = {
+  latest: "/latest",
+};
+
+export default routes;
