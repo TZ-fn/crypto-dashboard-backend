578560335c90155dbdaaecd35045fdddd2359cf6
Tomek <tomekz@vp.pl>
2023-06-24 17:16:54 +0200

    fix: fix a TS error

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ const APIroutes = {
 app.use((req, res, next) => {
   const corsWhitelist = ["https://crypto-dashboard-gamma-khaki.vercel.app", "http://localhost:5173"];
 
-  if (corsWhitelist.includes(req.headers.origin)) {
+  if (req.headers.origin && corsWhitelist.includes(req.headers.origin)) {
     res.header("Access-Control-Allow-Origin", req.headers.origin);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   }
